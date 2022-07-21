0014fffcea501a24b2b6038d5bb729484f6df779
Georgi Zlatev <gthebox@gmail.com>
2022-07-21 21:54:23 +0300

    first exam

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Makes-the-first-letter-uppercase-
+# Makes-the-first-letter-uppercase
+
+Makes the first letter uppercase, each subsequent letter lowercase of the entered name.
---
+++ index.js
@@ -0,0 +1,25 @@
+// 1 Name a let that stores the name user
+
+let name = prompt('What is your name');
+
+// 2 Capitalise the first letter of name
+
+// a first char
+
+let firstChar = name.slice(0,1);
+
+// b Turn the first char Upper case.
+
+let upperCaseFirstChar = firstChar.toUpperCase();
+
+// c Isolate the rest of the name.
+
+let restName = name.slice(1,name.length);
+
+// d concactenate the first char with rest of the char.
+
+let captionName = upperCaseFirstChar + restName;
+
+ // 3 We use the capitalised version ot the to greet them using an alret.
+
+alert('Hello ' + captionName);
